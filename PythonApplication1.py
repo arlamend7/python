3574367b5a3052c2da95e0dd976a41259cc9572c
arlamend7 <38164740+arlamend7@users.noreply.github.com>
2018-04-07 15:56:33 -0300

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -5,13 +5,13 @@ ids = ['pedra', 'papel', 'tissoura']
  
 random.choice(ids) 
 
-num = str(input('pedra, papel ou tissoura? \n')) 
+num = str(input('pedra, papel ou tesoura? \n')) 
 
 if num == random.choice(ids):
     print ('empate')
-elif num == 'pedra' and random.choice(ids) == 'papel' or num == 'tissoura' and random.choice(ids) == 'pedra' or num == 'papel' and random.choice(ids) == 'tissoura':
+elif num == 'pedra' and random.choice(ids) == 'papel' or num == 'tesoura' and random.choice(ids) == 'pedra' or num == 'papel' and random.choice(ids) == 'tesoura':
     print ('computador ganhou')
-elif num == 'papel' and random.choice(ids) == 'pedra' or num == 'pedra' and random.choice(ids) == 'tissoura' or num == 'tissoura' and random.choice(ids) == 'papel':
+elif num == 'papel' and random.choice(ids) == 'pedra' or num == 'pedra' and random.choice(ids) == 'tesoura' or num == 'tesoura' and random.choice(ids) == 'papel':
     print ('voce ganhou')
 else :
     print ('blz?')
