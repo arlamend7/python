549a0face49864eec3b4505f33d9f92986c0125a
arlamend7 <38164740+arlamend7@users.noreply.github.com>
2018-04-07 15:16:19 -0300

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,15 @@
+
+import random
+ 
+ids = ['pedra', 'papel', 'tissoura']
+ 
+random.choice(ids) 
+
+num = str(input('pedra, papel ou tissoura? \n')) 
+
+if num == random.choice(ids):
+    print ('empate')
+elif num == 'pedra' and random.choice(ids) == 'papel' or num == 'tissoura' and random.choice(ids) == 'pedra' or num == 'papel' and random.choice(ids) == 'tissoura':
+    print ('computador ganhou')
+elif num == 'papel' and random.choice(ids) == 'pedra' or num == 'pedra' and random.choice(ids) == 'tissoura' or num == 'tissoura' and random.choice(ids) == 'papel':
+    print ('voce ganhou')
